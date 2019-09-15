59135c0eaade6d154e223f75c94a78da77291e03
abantej <joeward.l.abante@gmail.com>
2019-09-15 19:59:00 +0800

    deleted .idea

--- app/src/main/java/com/abantej/kotlin/app/workout/tracker/MainActivity.kt
+++ app/src/main/java/com/abantej/kotlin/app/workout/tracker/MainActivity.kt
@@ -2,10 +2,16 @@ package com.abantej.kotlin.app.workout.tracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnShowToast.setOnClickListener {
+            Log.i("MainActivity", "Button was clicked!")
+        }
     }
 }
