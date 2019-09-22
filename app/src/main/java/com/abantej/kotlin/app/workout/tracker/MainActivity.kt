ccf1c585932410485ada260b58a6f304140b353f
abantej <joeward.l.abante@gmail.com>
2019-09-22 18:49:20 +0800

    added second activity

--- app/src/main/java/com/abantej/kotlin/app/workout/tracker/MainActivity.kt
+++ app/src/main/java/com/abantej/kotlin/app/workout/tracker/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.abantej.kotlin.app.workout.tracker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,8 +12,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnShowToast.setOnClickListener {
+        btnShowWorkouts.setOnClickListener {
             Log.i("MainActivity", "Button was clicked!")
+            Toast.makeText(this, "Button was clicked!", Toast.LENGTH_SHORT).show()
+        }
+
+        btnSendMsgToNextActivity.setOnClickListener {
+            val message: String = etUserMessage.text.toString()
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
         }
     }
 }
